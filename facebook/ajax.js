ac49984cf3f43e21170dd4546d3d4ade652f7f5b
Ameen Mohammed <ameen.mohammed95by@gmail.com>
2021-10-17 17:29:58 +0200

    Add some changes

--- facebook/ajax.js
+++ facebook/ajax.js
@@ -1,115 +1,94 @@
-$(function(){
-
-$('#login_button').click(function() {
-
- var email=$("#emailid").val();
- var pass=$("#password").val();
- if(email!="" && pass!="")
- {
-  $.ajax
-  ({
-  type:'post',
-  url:'do_login.php',
-  data:{
-	    do_login:"do_login",
-
-   id:email,
-   password:pass
-  },
-  success:function(response) {
-  if(response==1)
-  {
-    window.location.href="index.php";
-  }else if(response==2)
-  {
-
 $(document).ready(function(){
-    $("#text_login").text("please enter your email or mobile number");
- 
+    $('#login_button').click(function() {
+    
+     var email = $("#emailid").val();
+     var pass = $("#password").val();
+    
+     if(email !="" && pass !="")
+     {
+        $.ajax
+        ({
+            type:'post',
+            url:'do_login.php',
+            data:{
+              do_login: true,
+            email: email,
+            password: pass
+          },
+          success: function(response) {
+            if(response.trim() == 'ok'){
+              window.location.href = "index.php";
+            }else{
+              $("#text_login").text(response);
+            }
+          }
+        });
+     }else{
+        $("#text_login").text("please enter your email or mobile number");
+     }
+    })
+    
+    $('#sign_button').click(function() {
+    
+     var First_name = $("input[name=First_name]").val();
+     var Last_name = $("input[name=Last_name]").val();
+     var email = $("input[name=email]").val();
+     var pass = $("input[name=pass]").val();
+     var day = $("#day").find(":selected").text();
+     var year = $("#year").find(":selected").text();
+     var month = $("#month").find(":selected").text();
+     var gender=$("input[name=Gender]").val();
+    
+     if(email != "" && pass !="" && First_name != "" && Last_name!= "" && day !="" && month !="" && year !="")
+     {
+     
+      $.ajax
+      ({
+      type:'post',
+      url:'signup.php',
+      data:{
+        do_signup:"do_signup",
+       email:email,
+       password:pass,
+       day:day,
+       year:year,
+       month:month,
+       First_name:First_name,
+       Last_name:Last_name,
+       gender:gender
+      },
+      success:function(response) {
+          if(response==1)
+          {
+            window.location.href="index.php";
+          }
+          
+          else if(response==2)
+          {
+        $(document).ready(function(){
+            $("#text_sign").text("please fill all fields");
+        
+        });
+          }else if(response==3){
+          $(document).ready(function(){
+            $("#text_sign").text("Invalid email format");
+        
+        });
+          }else if(response==4){
+          $(document).ready(function(){
+            $("#text_sign").text("Password must be at least 8 characters in length and must contain at least one number, one upper case letter, one lower case letter and one special character.");
+        
+        });
+        }
+      }
+      });
+    
+     }
+    
+     return false;
+    });
+    
 });
-  }else if(response==3)
-  {
-
-$(document).ready(function(){
-    $("#text_login").text("please enter your password");
- 
-});
-  }else{
-	$(document).ready(function(){
-    $("#text_login").text("server error");
- 
-});
-}
-  }
-  });
- }
-
-
-
- return false;
-})
-
-$('#sign_button').click(function() {
-
- var First_name=$("input[name=First_name]").val();
- var Last_name=$("input[name=Last_name]").val();
- var email=$("input[name=email]").val();
- var pass=$("input[name=pass]").val();
- var day=$("#day").find(":selected").text();
- var year=$("#year").find(":selected").text();
- var month=$("#month").find(":selected").text();
- var gender=$("input[name=Gender]").val();
-
- if(email!="" && pass!=""&&First_name!=""&&Last_name!=""&&day!=""&&month!=""&&year!="")
- {
- 
-  $.ajax
-  ({
-  type:'post',
-  url:'signup.php',
-  data:{
-    do_signup:"do_signup",
-   email:email,
-   password:pass,
-   day:day,
-   year:year,
-   month:month,
-   First_name:First_name,
-   Last_name:Last_name,
-   gender:gender
-  },
-  success:function(response) {
-  if(response==1)
-  {
-    window.location.href="index.php";
-  }
-  
-  else if(response==2)
-  {
-$(document).ready(function(){
-    $("#text_sign").text("please fill all fields");
- 
-});
-  }else if(response==3){
-	$(document).ready(function(){
-    $("#text_sign").text("Invalid email format");
- 
-});
-  }else if(response==4){
-	$(document).ready(function(){
-    $("#text_sign").text("Password must be at least 8 characters in length and must contain at least one number, one upper case letter, one lower case letter and one special character.");
- 
-});
-}
-  }
-  });
-
- }
-
- return false;
-});
-});
-
 function disableBack() {
  window.history.forward();
   }
--- facebook/do_login.php
+++ facebook/do_login.php
@@ -5,39 +5,38 @@
 
     include_once 'Connection.php';
     try{
-    $database = new Connection();
-    $dbcon = $database->openConnection();
-    
+        $database = new Connection();
+        $dbcon = $database->openConnection();
+        
 
-    if(isset($_POST['do_login'])){
+        if(isset($_POST['do_login'])){
 
-        if(empty($_POST['id']))
-        {
-            echo 2;
-            die();
-        }else if (empty($_POST['password'])){
-            echo 3;
-            die();
+            if(empty($_POST['email']))
+            {
+                echo "pls enter email";
+                die();
+            }
+            
+            if(empty($_POST['pass']))
+            {
+                echo "pls enter password";
+                die();
+            }
+
+            $id = $_POST['id'];
+            $password = $_POST['password'];
+            $result = $dbcon->query("SELECT * FROM `users` WHERE `mobile`='$id' or `email`='$id' and `password`='$password'");
+            $number_of_rows = $result->fetchColumn();
+            
+            if ($number_of_rows > 0){
+                $_SESSION['client_id'] = $id;
+                echo 'ok';
+            }else{
+                echo 'user doesnt exist';
+            }
+            exit();
         }
-        
-        else{
-        $id=$_POST['id'];
-        $password=$_POST['password'];
-        $result = $dbcon->query("SELECT * FROM `users` WHERE `mobile`='$id' or `email`='$id' and `password`='$password'");
-        $number_of_rows = $result->fetchColumn();
-        
-        if ($number_of_rows>0){
-            $_SESSION['client_id']=$id;
-    	    echo 1;
-    	}
-    	else
-    	{
-    	    echo 0;
-    	}
-    	exit();
-    
-        }}
-    $database->closeConnection();
+        $database->closeConnection();
     
     }catch (PDOException $e)
     {
